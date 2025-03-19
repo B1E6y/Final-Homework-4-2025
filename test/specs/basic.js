558503c87e33d8e5b96ad3c5d79a0f3812b2d8a5
B1E6y <96926026+B1E6y@users.noreply.github.com>
2025-03-19 11:30:20 +0200

    Update basic.js

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -2,7 +2,7 @@ const searchActPage = require('../page/searchAct.page.js')
 
 
 
-describe("Should check subscribe feature (Final homework4)" , async () => {
+describe("Should find the attibute on Github website (Final homework4)" , async () => {
     
     it('Go to website github.com', async () => {
         await browser.url('https://github.com/');
@@ -10,7 +10,7 @@ describe("Should check subscribe feature (Final homework4)" , async () => {
         await browser.pause(100)
         })
 
-    it('Should go to Github, click on SignUp, set fake credentials, select country and check to exist expected text', async () => {
+    it('Should go to Github, set in Input text: "act" and click Enter. After should find an attribute, which has word "act"', async () => {
         await searchActPage.clickOnSearch();
         await browser.pause(100)
 
@@ -21,7 +21,6 @@ describe("Should check subscribe feature (Final homework4)" , async () => {
 
         await searchActPage.searchAct()
         await browser.pause(100)
-        // await expect(searchActPage.act).toHaveAttribute("href", '/Act', 'Act', '/act', 'act')
 
         })
     }
